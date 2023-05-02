02aba3e6d3c49c4c7f5aa7ed9cd74fbae3244738
Sebastian Quintero <ufoweb.co@gmail.com>
2023-05-02 12:52:10 -0500

    Clase 1
    Introducción

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let nombre = 'Sebastian';
+let apellido = "Quintero Ruiz";
+var edad = 34;
+const anioNacimiento = 1989;
+
+console.log(nombre);
+console.log(apellido);
+console.log(anioNacimiento);
